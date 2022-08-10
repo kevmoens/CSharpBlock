52db7b544c510a1fd13353b9ca551684eca7b882
Kevin Moens <kevmoens@yahoo.com>
2022-08-10 09:24:08 -0500

    add IronBlock and get C# from blocks

---
+++ CSharpBlock/Pages/Index.razor.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using IronBlock;
+using IronBlock.Blocks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Formatting;
+using Microsoft.CodeAnalysis.Text;
+using Microsoft.JSInterop;
+
+namespace CSharpBlock.Pages
+{
+	public partial class Index : ComponentBase
+	{
+
+        private IJSObjectReference? module;
+        internal class CustomPrintBlock : IBlock
+        {
+            public List<string> Text { get; set; } = new List<string>();
+
+            public override object Evaluate(Context context)
+            {
+                Text.Add((this.Values.FirstOrDefault(x => x.Name == "TEXT")?.Evaluate(context) ?? "").ToString());
+                return base.Evaluate(context);
+            }
+        }
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                module = await JS.InvokeAsync<IJSObjectReference>("import",
+                    "./javascript/blockly_ui_interop.js");
+                await module.InvokeVoidAsync("initialize", new object?[] { });
+            }
+            await base.OnAfterRenderAsync(firstRender);
+        }
+        [JSInvokable]
+        public static string Evaluate(string xml)
+        {
+            var printBlock = new CustomPrintBlock();
+
+            var parser =
+            new IronBlock.Parser()
+              .AddStandardBlocks()
+              .AddBlock("text_print", printBlock)
+              .Parse(xml);
+            var syntax = parser.Generate();
+            
+
+            parser.Evaluate();
+
+            return syntax.ToFullString() + '\n' + string.Join('\n', printBlock.Text);
+
+        }
+        public async Task OnRun()
+        {
+            if (module == null)
+            {
+                return;
+            }
+            await module.InvokeVoidAsync("evalProgram");
+        }
+    }
+}
+
---
+++ CSharpBlock/wwwroot/javascript/blockly_ui_interop.js
@@ -0,0 +1,26 @@
+﻿var workspace;
+export function initialize() {
+    workspace = Blockly.inject('blocklyDiv', {
+        media: 'media/',
+        toolbox: document.getElementById('toolbox')
+    });
+
+    Blockly.Xml.domToWorkspace(document.getElementById('defaultProgram'), workspace);
+}
+
+export function evalProgram() {
+    setOutput("");
+    var xml = Blockly.Xml.workspaceToDom(workspace);
+    var xmlText = new XMLSerializer().serializeToString(xml);
+    send(xmlText, setOutput)
+}
+
+export function setOutput(value) {
+    document.getElementById("output").innerText = value;
+}
+
+export function send(payload, cb) {
+    DotNet.invokeMethodAsync('CSharpBlock', 'Evaluate', payload)
+        .then(cb)
+        .catch(err => console.error(err))
+}
