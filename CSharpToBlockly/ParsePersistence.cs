32a78ea19e9b5caa60103037116737dcbe334351
Kevin <kmoens@lakeco.com>
2022-08-23 18:51:19 -0600

    Create persistence object used to avoid passing so many parameters

---
+++ CSharpToBlockly/ParsePersistence.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.CodeAnalysis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace CSharpToBlockly
+{
+    public class ParsePersistence
+    {
+        public XElement Document { get; set; } = new XElement("xml", "");
+        public SyntaxNode? TopNode { get; set; }
+        public Dictionary<ParsePersistenceLocation, ParsePersistenceDetail> Nodes { get; set; } = new ();
+        public HashSet<string> Variables { get; set; } = new HashSet<string>();
+        public void Reset()
+        {
+            Nodes = new Dictionary<ParsePersistenceLocation, ParsePersistenceDetail> ();
+            Variables = new HashSet<string> ();
+        }
+    }
+    
+    public class ParsePersistenceDetail
+    {
+        public XElement Doc { get; set; }
+        public XElement? LastNode { get; set; }
+        public SyntaxNode Node { get; set; }
+    }
+}
---
+++ CSharpToBlockly/ParsePersistenceLocation.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpToBlockly
+{
+    /// <summary>
+    /// Used to describe location in tree
+    /// 0
+    ///     First Node in tree
+    /// 0.0
+    ///     First Node under Top Node
+    /// 0.0.1
+    ///     Second Node under top node's top child
+    /// 0:0
+    ///     First token under first top node
+    ///     
+    /// </summary>
+    public record ParsePersistenceLocation
+    {
+        string _location;
+        public ParsePersistenceLocation(string location)
+        {
+            _location = location;
+        }
+
+        public override string ToString()
+        {
+            return _location;
+        }
+
+        public ParsePersistenceLocation GetParent()
+        {
+            string current = TruncateToken();
+            var items = current.Split(".");
+            if (items.Length == 0)
+            {
+                return new ParsePersistenceLocation("0");
+            }            
+            return new ParsePersistenceLocation(string.Join(".", items.Take(items.Length - 1)));
+        }
+
+        public ParsePersistenceLocation CreateChildNode(string childSuffix)
+        {
+            string current = TruncateToken();
+            return new ParsePersistenceLocation(current + $".{childSuffix}");
+        }
+        
+        private string TruncateToken()
+        {
+            var current = _location;
+            if (_location.IndexOf(":") > -1)
+            {
+                current = _location.Substring(0, _location.IndexOf(":"));
+            }
+
+            return current;
+        }
+    }
+}
